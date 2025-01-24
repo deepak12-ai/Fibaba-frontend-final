7d7701c4a51ce867aed477510cb6dd5778a8d94b
deepak12-ai <deepak5599666@gmail.com>
2025-01-24 13:22:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 import 'remixicon/fonts/remixicon.css'
 import { Provider } from 'react-redux'
-import { store } from './redux/Store.js'
+import { store } from './redux/store.js'
 
 
 createRoot(document.getElementById('root')).render(
